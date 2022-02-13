da234533d55639c0ff3520c7efa0b11d1265581d
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-14 00:13:16 +0530

    UC1

---
+++ UC1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AssignmentDay28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Adderess Book");
+            AssignmentDay28.UC1.GetContact();
+            AssignmentDay28.UC1.ListContact();
+        }
+    }
+}
