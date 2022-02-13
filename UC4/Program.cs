4443022519b338b3c659158a3831d102c6ec8c89
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-14 00:20:48 +0530

    UC4

---
+++ UC4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AssignmentDay28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Program");
+            Console.WriteLine("1.Enter to add the details\n2.Enter to modify the details");
+            Console.WriteLine("3.Listing the details..");
+            Console.WriteLine("4.Remove or Delete the Contact details");
+            Console.WriteLine("Enter a option");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    UC4.GetContact();
+                    UC4.ListContact();
+
+                    break;
+                case "2":
+                    UC4.GetContact();
+                    UC4.EditContact();
+                    UC4.ListContact();
+                    break;
+                case "3":
+                    UC4.GetContact();
+                    UC4.ListContact();
+                    break;
+                case "4":
+                    UC4.GetContact();
+                    UC4.DeletePeople();
+
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+            }
+        }
+    }
+}
